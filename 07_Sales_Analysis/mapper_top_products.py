965a271e8356f2a45a90394a8b4538334fc132b8
Mahi Satani <mahisatani21@gmail.com>
2026-07-15 00:18:10 +0530

    Add files via upload

---
+++ 07_Sales_Analysis/mapper_top_products.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+
+    if not line:
+        continue
+
+    parts = line.split(',')
+
+    if len(parts) != 4:
+        continue
+
+    try:
+        date = parts[0]
+        product = parts[1]
+        price = float(parts[2])
+        city = parts[3]
+
+        print(f"{product}\t{price}")
+
+    except ValueError:
+        continue
